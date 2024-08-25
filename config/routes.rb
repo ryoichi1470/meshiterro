6443faf786b07bb34ea4bcf6d2925fb4905b2cc1
Ryoichiro Kitahara <175901292+ryoichi1470@users.noreply.github.com>
2024-08-25 10:16:27 +0000

    [finish11-14]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
   devise_for :users
   root to: 'homes#top'
   get 'homes/about', to: 'homes#about', as: 'about'
+  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:show, :edit]
 end
